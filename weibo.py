26a92336910bbbd07f0a2a8ab670426e58f54010
“dataabc <chillychen1991@gmail.com>
2020-02-01 12:44:33 +0800

    perf: 优化文件下载
    下载图片、视频等也和获取文本信息一样，每隔几页就下载（而不是集中下载）
    
    Issue #30

--- weibo.py
+++ weibo.py
@@ -314,7 +314,7 @@ class Weibo(object):
                 file_path = file_dir + os.sep + file_name
                 self.download_one_file(urls, file_path, file_type, w['id'])
 
-    def download_files(self, file_type, weibo_type):
+    def download_files(self, file_type, weibo_type, wrote_count):
         """下载文件(图片/视频)"""
         try:
             describe = ''
@@ -333,7 +333,7 @@ class Weibo(object):
             file_dir = file_dir + os.sep + describe
             if not os.path.isdir(file_dir):
                 os.makedirs(file_dir)
-            for w in tqdm(self.weibo, desc='Download progress'):
+            for w in tqdm(self.weibo[wrote_count:], desc='Download progress'):
                 if weibo_type == 'retweet':
                     if w.get('retweet'):
                         w = w['retweet']
@@ -895,6 +895,15 @@ class Weibo(object):
                 self.weibo_to_mysql(wrote_count)
             if 'mongo' in self.write_mode:
                 self.weibo_to_mongodb(wrote_count)
+            if self.original_pic_download:
+                self.download_files('img', 'original', wrote_count)
+            if self.original_video_download:
+                self.download_files('video', 'original', wrote_count)
+            if not self.filter:
+                if self.retweet_pic_download:
+                    self.download_files('img', 'retweet', wrote_count)
+                if self.retweet_video_download:
+                    self.download_files('video', 'retweet', wrote_count)
 
     def get_pages(self):
         """获取全部微博"""
@@ -961,15 +970,6 @@ class Weibo(object):
                 print('*' * 100)
                 if self.user_config_file_path:
                     self.update_user_config_file(self.user_config_file_path)
-                if self.original_pic_download:
-                    self.download_files('img', 'original')
-                if self.original_video_download:
-                    self.download_files('video', 'original')
-                if not self.filter:
-                    if self.retweet_pic_download:
-                        self.download_files('img', 'retweet')
-                    if self.retweet_video_download:
-                        self.download_files('video', 'retweet')
         except Exception as e:
             print('Error: ', e)
             traceback.print_exc()
