926622611ec2884ab72fecfe0a315b24b1a196cc
dataabc <chillychen1991@gmail.com>
2020-06-01 21:18:11 +0800

    perf: 优化config.json文件读取

--- weibo.py
+++ weibo.py
@@ -1079,19 +1079,25 @@ class Weibo(object):
             traceback.print_exc()
 
 
+def get_config():
+    """获取config.json文件信息"""
+    config_path = os.path.split(
+        os.path.realpath(__file__))[0] + os.sep + 'config.json'
+    if not os.path.isfile(config_path):
+        sys.exit(u'当前路径：%s 不存在配置文件config.json' %
+                 (os.path.split(os.path.realpath(__file__))[0] + os.sep))
+    try:
+        with open(config_path) as f:
+            config = json.loads(f.read())
+            return config
+    except ValueError:
+        sys.exit(u'config.json 格式不正确，请参考 '
+                 u'https://github.com/dataabc/weibo-crawler#3程序设置')
+
+
 def main():
     try:
-        config_path = os.path.split(
-            os.path.realpath(__file__))[0] + os.sep + 'config.json'
-        if not os.path.isfile(config_path):
-            sys.exit(u'当前路径：%s 不存在配置文件config.json' %
-                     (os.path.split(os.path.realpath(__file__))[0] + os.sep))
-        with open(config_path) as f:
-            try:
-                config = json.loads(f.read())
-            except ValueError:
-                sys.exit(u'config.json 格式不正确，请参考 '
-                         u'https://github.com/dataabc/weibo-crawler#3程序设置')
+        config = get_config()
         wb = Weibo(config)
         wb.start()  # 爬取微博信息
     except Exception as e:
