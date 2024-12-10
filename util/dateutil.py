ae6f3bfc32188457c3ae1361072f881f24b957e3
LuChao <atorber@163.com>
2024-12-10 19:46:58 +0800

    修复时间格式问题

--- util/dateutil.py
+++ util/dateutil.py
@@ -2,8 +2,14 @@
 import datetime
 
 
+# def convert_to_days_ago(date_str, how_many_days):
+#     """将日期字符串转换为多少天前的日期字符串"""
+#     date_str = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+#     date_str = date_str + datetime.timedelta(days=-how_many_days)
+#     return date_str.strftime('%Y-%m-%d')
+
 def convert_to_days_ago(date_str, how_many_days):
     """将日期字符串转换为多少天前的日期字符串"""
-    date_str = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    date_str = datetime.datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
     date_str = date_str + datetime.timedelta(days=-how_many_days)
-    return date_str.strftime('%Y-%m-%d')
+    return date_str.strftime('%Y-%m-%dT%H:%M:%S')
