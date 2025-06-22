27e1fc2dac8e2260fa8b521b8f1275610f8fd3ab
Heng Li <github@hengli.me>
2025-06-22 17:24:10 +0800

    adding header for image,video download

--- weibo.py
+++ weibo.py
@@ -104,6 +104,7 @@ class Weibo(object):
                 key, value = pair.split('=', 1)
                 cookies[key.strip()] = value.strip()
         self.headers = {
+            'Referer': 'https://weibo.com/',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
             'cache-control': 'max-age=0',
