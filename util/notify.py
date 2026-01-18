d13b8b790b916ee8ae1ec12f6d2b1763bf7ed554
Troray <troraycom@gmail.com>
2026-01-19 00:07:18 +0800

    新增保存为markdown格式

--- util/notify.py
+++ util/notify.py
@@ -7,5 +7,5 @@ def push_deer(append_str):
         'pushkey': const.NOTIFY['PUSH_KEY'],
         'text': append_str,
     }
-    # 使用HTTPS，如果在这个环境中遇到证书问题，可以设置 verify=False
-    requests.get(url="https://api2.pushdeer.com/message/push", params=params, verify=False)
+    # 使用HTTPS请求发送通知
+    requests.get(url="https://api2.pushdeer.com/message/push", params=params)
